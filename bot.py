fb56edb8c51ea383bab167d68095c1fca174e72e
godofmischief22 <aadionline23@gmail.com>
2025-04-30 13:24:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,4 @@
-import discord
-from discord.ext import commands
-import os
+if __name__ == "__main__":
+    from core import bot
 
-TOKEN = os.getenv("DISCORD_TOKEN")
-
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix="q", intents=intents)
-
-@bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user}")
-
-@bot.command()
-async def hello(ctx):
-    await ctx.send("Hello! I'm alive.")
-
-bot.run(TOKEN)
+    bot.run(bot.config.DISCORD_TOKEN)
